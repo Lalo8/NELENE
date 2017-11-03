692c82b6fd1c98ff09661c610b6505468e43787d
Elo <wanang.elodie@hotmail.fr>
2017-11-03 19:17:49 +0100

    fixed production base URL

--- myvueBulma/src/api/organisations.js
+++ myvueBulma/src/api/organisations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? '/api' : "http://localhost:3000/api/organisations",
+  baseURL: process.env.NODE_ENV === "production" ? '/api/organisations' : "http://localhost:3000/api/organisations",
 });
 
 export function getOrganisations() {
