e1afac77c0c922578433e535b1e92922c08043c6
Elo <wanang.elodie@hotmail.fr>
2017-11-06 16:52:34 +0100

    Use render function in main

--- myvueBulma/src/main.js
+++ myvueBulma/src/main.js
@@ -1,34 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Buefy from 'buefy';
-import * as VueGoogleMaps from 'vue2-google-maps'
-import Vue2Filters from 'vue2-filters'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Buefy from "buefy";
+import * as VueGoogleMaps from "vue2-google-maps";
+import Vue2Filters from "vue2-filters";
 
-import 'buefy/lib/buefy.css';
+import "buefy/lib/buefy.css";
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyDuBacGwzcYTG2tpXRlcTc2d6mYk2MZF0c',
-    libraries: 'places',
-  },
+    key: "AIzaSyDuBacGwzcYTG2tpXRlcTc2d6mYk2MZF0c",
+    libraries: "places"
+  }
 });
 
-Vue.use(Buefy,{
-  defaultIconPack:'fa'
+Vue.use(Buefy, {
+  defaultIconPack: "fa"
 });
 Vue.use(Vue2Filters);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   data: {
-    user:null,
+    user: null
   },
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App)
+});
